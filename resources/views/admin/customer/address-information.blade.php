d0b948ff5e97143ad1f9a9f52eb68289f57357b4
vietlq57 <luongviet82@gmail.com>
2018-09-13 22:42:16 +0700

    address information tab

---
+++ resources/views/admin/customer/address-information.blade.php
@@ -0,0 +1,142 @@
+<div class="panel panel-default">
+    <div class="panel-body">
+        <div class="row">
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>Delivery Name</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <input type="text" class="form-control" value="">
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>Delivery Address</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <input type="text" class="form-control" value="">
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>City Delivery</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <select class="form-control">
+                        <option value="1">Select an Option</option>
+                    </select>
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>District Delivery</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <select class="form-control">
+                        <option value="1">Select an Option</option>
+                    </select>
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>Invoice Name</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <input type="text" class="form-control" value="">
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>Invoice Address</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <input type="text" class="form-control" value="">
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>City Invoice</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <select class="form-control">
+                        <option value="1">Select an Option</option>
+                    </select>
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>District Invoice</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <select class="form-control">
+                        <option value="1">Select an Option</option>
+                    </select>
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>Default Warehouse</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <select class="form-control">
+                        <option value="1">Select an Option</option>
+                    </select>
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>BackOrder Priority</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <select class="form-control">
+                        <option value="1">Select an Option</option>
+                    </select>
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>Customer Physical Branch</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <select class="form-control">
+                        <option value="1">Select an Option</option>
+                    </select>
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>License Code</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <input type="text" class="form-control" value="">
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>Route Code</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <input type="text" class="form-control" value="">
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>Delivery On Saturday</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <select class="form-control">
+                        <option value="1">Select an Option</option>
+                    </select>
+                </div>
+            </div>
+            <div class="section">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <label>Customer Code</label>
+                </div>
+                <div class="col-md-9 col-sm-9 col-xs-9">
+                    <input type="text" class="form-control" value="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
